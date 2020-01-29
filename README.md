e5796d9dbb1f8009faf4ffd2d75f8b9d135ee85b
Lucy20209060 <550502661@qq.com>
2020-01-29 11:54:35 +0800

    Update README.md

--- README.md
+++ README.md
@@ -8,12 +8,40 @@
 示例
 ------
 ```javascript
-import TextEdit from 'react-text-edit'
 /**
  * value      初始值
  * disabled   是否禁用编辑按钮
  * editText   编辑按钮文字 默认 “编辑”
  * onOk(value, callback)      确定按钮回调函数
  */
+import React, { useState } from 'react';
+import TextEdit from 'react-text-edit'
+
+function App() {
+	const [text, setText] = useState('react-text-edit');
+	
+	return (
+		<div>
+			<h1>react-text-edit 示例</h1>
+			<TextEdit 
+				editText='修改' 
+				maxLength={20} 
+				value={text} 
+				disabled={false} 
+				onOk={(value, callback) => editOk(value, callback)} 
+				onCancel={() => console.log('onCancel')}
+			/>
+		</div>
+	)
+
+	function editOk(value, callback) {
+		setText(value)
+		callback.success()
+		console.log('editOk')
+	}
+    
+}
+
+export default App;
 
 ```
--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,26 @@ function TextEdit(props) {
   const [inputValue, setInputValue] = useState(value);
   const [editStatus, setEditStatus] = useState(false);
 
-  function inputChange(val) {
+  return editStatus ? (
+    <div className='text-editing-wrap'>
+      <input value={inputValue} maxLength={maxLength} onChange={(val)=> inputChange(val.target.value)} />
+      <p>
+        <button className='text-editing-button-ok' onClick={()=> ok()}>确定</button>
+        <button className='text-editing-button-cancel' onClick={()=> cancel()}>取消</button>
+      </p>
+    </div>
+  ) : (
+    <div className='text-edit-wrap'>
+      <span className='text'>{inputValue}</span>
+      <button
+        className='edit-button'
+        onClick={()=>setEditStatus(true)}
+        disabled={disabled}
+      >{editText}</button>
+    </div>
+	)
+	
+	function inputChange(val) {
     setInputValue(val)
   }
 
@@ -30,25 +49,6 @@ function TextEdit(props) {
       }
     )
   }
-
-  return editStatus ? (
-    <div className='text-editing-wrap'>
-      <input value={inputValue} maxLength={maxLength} onChange={(val)=> inputChange(val.target.value)} />
-      <p>
-        <button className='text-editing-button-ok' onClick={()=> ok()}>确定</button>
-        <button className='text-editing-button-cancel' onClick={()=> cancel()}>取消</button>
-      </p>
-    </div>
-  ) : (
-    <div className='text-edit-wrap'>
-      <span className='text'>{inputValue}</span>
-      <button
-        className='edit-button'
-        onClick={()=>setEditStatus(true)}
-        disabled={disabled}
-      >{editText}</button>
-    </div>
-  )
 }
 
 export default TextEdit;
